a4c6932db4054bb5754ecb893d83c9d70a586e05
iamakeem <bolarinwa_akeem@yahoo.com>
2023-03-26 17:55:44 +0100

    header done

---
+++ src/component/Header.jsx
@@ -0,0 +1,85 @@
+// import  from 'react'
+import React from 'react';
+
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  Image,
+  Text,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const Links = ['Upload Photo(s)', 'Buy Photo(s)', 'Team'];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}
+  >
+    {children}
+  </Link>
+);
+
+export default function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box bg={useColorModeValue('blue.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <IconButton
+          size={'md'}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={'Open Menu'}
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8} alignItems={'center'}>
+          <Box>
+            <Image
+              src='picParadise1.png'
+              alt='logo'
+              height={50}
+              width={50}
+              borderRadius='24px 0 30px 0'
+            />
+          </Box>
+          <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+            {Links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems={'center'}>
+          {/* <Menu>
+            </Menu> */}
+          <Button>
+            <Text>Connect Wallet</Text>
+          </Button>
+        </Flex>
+      </Flex>
+      {isOpen ? (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as={'nav'} spacing={4}>
+            {Links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
 import chakraTheme from '@chakra-ui/theme';
-
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+
+const { Button } = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
