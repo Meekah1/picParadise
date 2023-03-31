59d1b25719cab1a6f125ae42b2cb9cf827cf5672
iamakeem <bolarinwa_akeem@yahoo.com>
2023-03-31 19:53:35 +0100

    sort

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Flex } from '@chakra-ui/react';
 import Header from '../component/Header';
-import HeroSection from './../component/HeroSection';
+import HeroSection from '../component/HeroSection';
 
 export default function Home() {
   return (
